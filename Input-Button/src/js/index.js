fec451baaa406b389480a2acb604a7ea3508c1e6
AnnaGorbachev <anna.gorbacheva231@gmail.com>
2022-12-11 12:50:07 +0200

    radio

--- Input-Button/src/js/index.js
+++ Input-Button/src/js/index.js
@@ -1,6 +1,52 @@
-// $(document).ready(function () {
+// Стилизация чекбоксов и радиокнопок на CSS HTML jQuery
 
-// });
+$(document).ready(function () {
+  // CHECKBOX--если выделен то доб-ть .active
+  $.each($('.checkbox'), function (index, val) {
+    if ($(this).find('input').prop('checked') == true) {
+      $(this).addClass('active');
+    }
+  });
+  // событие click  на checkbox
+  // если у наж-го div.checkbox есть .active то убираем активность c input и наоборот
+  $(document).on('click', '.checkbox', function (event) {
+    if ($(this).hasClass('active')) {
+      $(this).find('input').prop('checked', false);
+    } else {
+      $(this).find('input').prop('checked', true);
+    }
+    // доб-ем или убираем .active для самого нажатого div.checkbox
+    $(this).toggleClass('active');
+
+    return false;
+  })
+
+
+  // RADIO BUTTONS
+  // при наж на р-ку ищем родителя .radiobuttons,
+  // потом ищем все р-ки в нем,уб у них .active
+  // а также уб у всех input выделенное зн-е т е checked
+  // и доб-ем active только нажатому эл-ту
+  // и в нем ищем input и доб-ем ему checked
+  $.each($('.radiobuttons__item'), function (index, val) {
+    if ($(this).find('input').prop('checked') == true) {
+      $(this).addClass('active');
+    }
+  });
+
+  $(document).on('click', '.radiobuttons__item', function (event) {
+    $(this).parents('.radiobuttons').find('.radiobuttons__item').removeClass('active');
+    $(this).parents('.radiobuttons').find('.radiobuttons__item input').prop('checked', false);
+    $(this).toggleClass('active');
+    $(this).find('input').prop('checked', true);
+    return false;
+  })
+
+
+
+
+
+});
 
 
 
