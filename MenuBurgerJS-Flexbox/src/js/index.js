f9d880da7ee8fb1e8f070bd861a0877f4acde8c7
AnnaGorbachev <anna.gorbacheva231@gmail.com>
2022-12-23 09:43:01 +0200

    menu-burger

--- MenuBurgerJS-Flexbox/src/js/index.js
+++ MenuBurgerJS-Flexbox/src/js/index.js
@@ -46,6 +46,19 @@ if (isMobile.any()) {
   document.body.classList.add('_pc');
 }
 
+//Меню бургер
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+
+if (iconMenu) {
+
+  iconMenu.addEventListener('click', function (e) {
+    document.body.classList.toggle('_lock');//убираем скролл при открытом меню
+    iconMenu.classList.toggle('_active');
+    menuBody.classList.toggle('_active');
+  });
+}
+
 // прокрутка при клике
 
 const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
@@ -69,8 +82,15 @@ if (menuLinks.length > 0) {
 
       // gotoBlockValue-точное положен этого об с уч высоты шапки
       // встр ф-я getBoundingClientRect().top -расст от верха в px + кол-во прокруч px - высота шапки
-      const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+      const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector('header').offsetHeight;
 
+
+      if (iconMenu.classList.contains('_active')) {//если iconMenu содержит _active т.е если меню открыто
+        document.body.classList.remove('_lock');//то убираем классы кот мы доб-ли при открытии его
+        iconMenu.classList.remove('_active');
+        menuBody.classList.remove('_active');
+
+      }
 
       // это заставит кусочек прокрутиться
       window.scrollTo({
@@ -86,6 +106,9 @@ if (menuLinks.length > 0) {
   }
 
 }
+
+
+
 
 
 // для разработчика-чт автоматич присваивался класс
