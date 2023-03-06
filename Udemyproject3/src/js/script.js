52fccb70e15635d402e57a9b102d30a2fcc0ff83
AnnaGorbachev <anna.gorbacheva231@gmail.com>
2023-03-06 10:04:31 +0200

    calculator

--- Udemyproject3/src/js/script.js
+++ Udemyproject3/src/js/script.js
@@ -283,8 +283,6 @@ window.addEventListener('DOMContentLoaded', () => {
     bindPostData(item);
   });
 
-
-
   const postData = async (url, data) => {
     let res = await fetch(url, {
       method: 'POST',
@@ -295,8 +293,6 @@ window.addEventListener('DOMContentLoaded', () => {
     });
     return await res.json();
   };
-
-
 
   function bindPostData(form) {
     form.addEventListener('submit', (e) => {
@@ -314,7 +310,6 @@ window.addEventListener('DOMContentLoaded', () => {
       // превращ в массив массивов(formData.entries()),потом в обьект(Object.fromEntries), и в json
 
       const json = JSON.stringify(Object.fromEntries(formData.entries()));
-
 
 
       postData('http://localhost:3000/requests', json)
@@ -468,7 +463,18 @@ window.addEventListener('DOMContentLoaded', () => {
   //---Slider---------------
   // offer__slider-inner--обернули слайдер в этот блок чт это было окошко через кот б.видеть текущий слайд
 
+  //---------Точки для слайдера
+  //-Алгоритм:
+  //1.получаем как элемент весь слайдер а не только враппер
+  //2.устан ему position relative если у него такого нет т к точки будут абсол спозиц-ны и б прикрепл к низу слайдера
+  //3.создаем обертку для этих точек
+  //4.с пом цикла создаем кол-во точек == кол-ву слайдов
+  //5.причем каждой точке надо присв атрибут для связи (первая точка-это первый сл-р и тд)
+  //6.сделать класс активности для активной точки
+  //7.сделать чтобы класс активности менялся на точках при передв слайдера
+
   const slides = document.querySelectorAll('.offer__slide'),
+    slider = document.querySelector('.offer__slider'),//1.точки
     prev = document.querySelector('.offer__slider-prev'),
     next = document.querySelector('.offer__slider-next'),
     total = document.querySelector('#total'),//общее кол-во
@@ -498,14 +504,64 @@ window.addEventListener('DOMContentLoaded', () => {
   slides.forEach(slide => {//т к слайды м б разной шир--устан для них одну ширину
     slide.style.width = width;
   });
+  //
+
+  slider.style.position = 'relative';//2.точки
+
+  const indicators = document.createElement('ol'),//3.точки
+    dots = [];//7.точки
+  indicators.classList.add('carousel-indicators');
+  indicators.style.cssText = `
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 15;
+        display: flex;
+        justify-content: center;
+        margin-right: 15%;
+        margin-left: 15%;
+        list-style: none;
+    `; // Если хотите - добавьте в стили, но иногда у нас нет доступа к стилям
+  slider.append(indicators);
+
+  for (let i = 0; i < slides.length; i++) {//4.точки
+    const dot = document.createElement('li');
+    dot.setAttribute('data-slide-to', i + 1);//5.точки
+    dot.style.cssText = `
+            box-sizing: content-box;
+            flex: 0 1 auto;
+            width: 30px;
+            height: 6px;
+            margin-right: 3px;
+            margin-left: 3px;
+            cursor: pointer;
+            background-color: #fff;
+            background-clip: padding-box;
+            border-top: 10px solid transparent;
+            border-bottom: 10px solid transparent;
+            opacity: .5;
+            transition: opacity .6s ease;
+        `;
+    if (i == 0) {//6.точки
+      dot.style.opacity = 1;
+    }
+    indicators.append(dot);
+    dots.push(dot);//7 точки
+  }
+
+
+  function deleteNotDigits(str) {
+    return +str.replace(/\D/g, '');
+  }
 
   next.addEventListener('click', () => {
 
-    if (offset == (+width.slice(0, width.length - 2) * (slides.length - 1))) {
+    if (offset == deleteNotDigits(width) * (slides.length - 1)) { //slice(0, width.length - 2)заменила на replace(/\D/g, '')
       offset = 0;//значит мы долистали до конца и надо вернуться в самое начало
       // (+width.slice(0, width.length - 2)--'500px'переделываем в 500
     } else {
-      offset += +width.slice(0, width.length - 2);//добавляем смещение
+      offset += deleteNotDigits(width);//добавляем смещение
     }
 
     //сдвигаем слайд
@@ -522,13 +578,17 @@ window.addEventListener('DOMContentLoaded', () => {
     } else {
       current.textContent = slideIndex;
     }
-  })
+
+    dots.forEach(dot => dot.style.opacity = ".5"); //изначально ставим точкам
+    dots[slideIndex - 1].style.opacity = 1;//.6
+
+  });
 
   prev.addEventListener('click', () => {
     if (offset == 0) {
-      offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+      offset = deleteNotDigits(width) * (slides.length - 1);
     } else {
-      offset -= +width.slice(0, width.length - 2);
+      offset -= deleteNotDigits(width);
     }
 
     slidesField.style.transform = `translateX(-${offset}px)`;
@@ -544,12 +604,150 @@ window.addEventListener('DOMContentLoaded', () => {
     } else {
       current.textContent = slideIndex;
     }
+
+    dots.forEach(dot => dot.style.opacity = ".5");//.6
+    dots[slideIndex - 1].style.opacity = 1;
+
   });
 
 
+  dots.forEach(dot => { //.7
+    dot.addEventListener('click', (e) => {
+      const slideTo = e.target.getAttribute('data-slide-to');
+
+      slideIndex = slideTo;
+      offset = deleteNotDigits(width) * (slideTo - 1);
+
+      slidesField.style.transform = `translateX(-${offset}px)`;
+
+      if (slides.length < 10) {
+        current.textContent = `0${slideIndex}`;
+      } else {
+        current.textContent = slideIndex;
+      }
+
+      dots.forEach(dot => dot.style.opacity = ".5");
+      dots[slideIndex - 1].style.opacity = 1;
+    });
+  });
+
+  //-------Калькулятор-----------
+
+  const result = document.querySelector('.calculating__result span');
+  let sex,
+    height,
+    weight,
+    age,
+    ratio;
+
+  if (localStorage.getItem('sex')) {//проверка естьли что-то в localStorage
+    sex = localStorage.getItem('sex');
+  } else {
+    sex = 'female';//для расчета по умолч
+    localStorage.setItem('sex', 'female');
+  }
+
+  if (localStorage.getItem('ratio')) {
+    ratio = localStorage.getItem('ratio');
+  } else {
+    ratio = 1.375;//для расчета по умолч
+    localStorage.setItem('ratio', 1.375);
+  }
+
+  function initLocalSettings(selector, activeClass) {// когда зах на стр чт подтягивались данные из localStorage
+    const elements = document.querySelectorAll(selector);
+
+    elements.forEach(elem => {
+      elem.classList.remove(activeClass);//уд класс активности
+      if (elem.getAttribute('id') === localStorage.getItem('sex')) {
+        elem.classList.add(activeClass);
+      }
+      if (elem.getAttribute('data-ratio') === localStorage.getItem('ratio')) {
+        elem.classList.add(activeClass);
+      }
+    });
+  }
+
+  initLocalSettings('#gender div', 'calculating__choose-item_active');
+  initLocalSettings('.calculating__choose_big div', 'calculating__choose-item_active');
+
+  function calcTotal() {
+    //проверка заполнены ли все данные
+    if (!sex || !height || !weight || !age || !ratio) {
+      result.textContent = '____';
+      return;
+    }
+    if (sex === 'female') {
+      result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);//формула из сайта
+    } else {
+      result.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio);
+    }
+  }
+  calcTotal();
 
 
 
+  function getStaticInformation(selector, activeClass) { //ф-я для двух блоков div
+    const elements = document.querySelectorAll(selector);
+    //дилегирование событий не подх т к будет нажиматься и между ними?
+    //поэт просто перебираем эл-ты и на каж вешаем обработчик
+    elements.forEach(elem => {
+      elem.addEventListener('click', (e) => {
+        //вытаскиваем значение кнопки на кот кликнули
+        if (e.target.getAttribute('data-ratio')) {
+          ratio = +e.target.getAttribute('data-ratio');
+          localStorage.setItem('ratio', +e.target.getAttribute('data-ratio'));
+        } else {
+          sex = e.target.getAttribute('id');
+          localStorage.setItem('sex', e.target.getAttribute('id'));
+        }
+        //классы активности--сначала убираем все классы акт а потом назначаем одному
+        elements.forEach(elem => {
+          elem.classList.remove(activeClass);
+        });
+
+        e.target.classList.add(activeClass);
+
+        calcTotal();
+      });
+    });
+  }
+
+  getStaticInformation('#gender div', 'calculating__choose-item_active');
+  getStaticInformation('.calculating__choose_big div', 'calculating__choose-item_active');
+
+  function getDynamicInformation(selector) { //ф-я для инпутов
+    const input = document.querySelector(selector);
+
+    input.addEventListener('input', () => {
+      //если поль-ль ввел не число то делаем красным border
+      if (input.value.match(/\D/g)) { //если не число
+        input.style.border = "1px solid red";
+      } else {
+        input.style.border = 'none';
+      }
+
+      switch (input.getAttribute('id')) {// проверяем какой именно инпут нажат
+
+        case "height":
+          height = +input.value;
+          break;
+        case "weight":
+          weight = +input.value;
+          break;
+        case "age":
+          age = +input.value;
+          break;
+
+      }
+      calcTotal();
+    })
+
+  }
+
+  getDynamicInformation('#height');
+  getDynamicInformation('#weight');
+  getDynamicInformation('#age');
 
 
   //-------простой слайдер-----------
@@ -603,5 +801,91 @@ window.addEventListener('DOMContentLoaded', () => {
   // next.addEventListener('click', () => {
   //   plusSlides(1);
   // })
+
+
+  //--------------Слайдер-карусель без точек------------------------
+
+  // const slides = document.querySelectorAll('.offer__slide'),
+  //   prev = document.querySelector('.offer__slider-prev'),
+  //   next = document.querySelector('.offer__slider-next'),
+  //   total = document.querySelector('#total'),//общее кол-во
+  //   current = document.querySelector('#current'),
+  //   slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+  //   width = window.getComputedStyle(slidesWrapper).width,//получаем ширину враппера
+  //   slidesField = document.querySelector('.offer__slider-inner');//строка для всей карусели
+
+  // let slideIndex = 1;
+  // let offset = 0;//отступ
+
+
+  // if (slides.length < 10) {
+  //   total.textContent = `0${slides.length}`;//общее кол-во
+  //   current.textContent = `0${slideIndex}`;//номер текущего слайда
+  // } else {
+  //   total.textContent = slides.length;
+  //   current.textContent = slideIndex;
+  // }
+
+  // slidesField.style.width = 100 * slides.length + '%';//ширина для иннера
+  // slidesField.style.display = 'flex';
+  // slidesField.style.transition = '0.5s all';//стили для слайдов-чт они выстроились в ряд
+
+  // slidesWrapper.style.overflow = 'hidden';//скрываем все слайды вне враппера
+
+  // slides.forEach(slide => {//т к слайды м б разной шир--устан для них одну ширину
+  //   slide.style.width = width;
+  // });
+
+  // next.addEventListener('click', () => {
+
+  //   if (offset == (+width.slice(0, width.length - 2) * (slides.length - 1))) {
+  //     offset = 0;//значит мы долистали до конца и надо вернуться в самое начало
+  //     // (+width.slice(0, width.length - 2)--'500px'переделываем в 500
+  //   } else {
+  //     offset += +width.slice(0, width.length - 2);//добавляем смещение
+  //   }
+
+  //   //сдвигаем слайд
+  //   slidesField.style.transform = `translateX(-${offset}px)`;
+
+  //   if (slideIndex == slides.length) {//если мы на последнем-то перемещ-ся на первый
+  //     slideIndex = 1;
+  //   } else {
+  //     slideIndex++;
+  //   }
+
+  //   if (slides.length < 10) {
+  //     current.textContent = `0${slideIndex}`;
+  //   } else {
+  //     current.textContent = slideIndex;
+  //   }
+  // })
+
+  // prev.addEventListener('click', () => {
+  //   if (offset == 0) {
+  //     offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+  //   } else {
+  //     offset -= +width.slice(0, width.length - 2);
+  //   }
+
+  //   slidesField.style.transform = `translateX(-${offset}px)`;
+
+  //   if (slideIndex == 1) {
+  //     slideIndex = slides.length;
+  //   } else {
+  //     slideIndex--;
+  //   }
+
+  //   if (slides.length < 10) {
+  //     current.textContent = `0${slideIndex}`;
+  //   } else {
+  //     current.textContent = slideIndex;
+  //   }
+  // });
+
+
+
+
+
 
 });
