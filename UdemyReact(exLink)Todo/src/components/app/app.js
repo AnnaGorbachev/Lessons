01a8ca840fdf12864bf6a037a9c6bdd7b17b7b04
AnnaGorbachev <anna.gorbacheva231@gmail.com>
2023-04-27 18:53:10 +0300

    form

--- UdemyReact(exLink)Todo/src/components/app/app.js
+++ UdemyReact(exLink)Todo/src/components/app/app.js
@@ -59,17 +59,46 @@ class App extends Component {
   }
 
   onToggleIncrease = (id) => {
-    console.log(`increase = ${id}`);
+    // this.setState(({ data }) => {
+    //   const index = data.findIndex(elem => elem.id === id);
+    //   const old = data[index];
+    //   const newItem = { ...old, increase: !old.increase };
+    //   const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
+    //   return {
+    //     data: newArr
+    //   }
+    // })
+
+    this.setState(({ data }) => ({
+      data: data.map(item => {
+        if (item.id === id) {
+          return { ...item, increase: !item.increase }
+        }
+        return item;
+      })
+    }))
+
   }
 
   onToggleRise = (id) => {
-    console.log(`rise = ${id}`);
+    this.setState(({ data }) => ({
+      data: data.map(item => {
+        if (item.id === id) {
+          return { ...item, rise: !item.rise }
+        }
+        return item;
+      })
+    }))
   }
 
   render() {
+
+    const employers = this.state.data.length;
+    const increased = this.state.data.filter(item => item.increase).length;
+
     return (
       <div className="app">
-        <AppInfo />
+        <AppInfo employers={employers} increased={increased} />
 
         <div className="search-panel">
           <SearchPanel />
--- UdemyReact(exLink)Todo/src/components/employers-add-form/employers-add-form.js
+++ UdemyReact(exLink)Todo/src/components/employers-add-form/employers-add-form.js
@@ -1,5 +1,6 @@
 
 import { Component } from 'react';
+// import { useId } from 'react';
 
 import './employers-add-form.css';
 
@@ -21,6 +22,7 @@ class EmployersAddForm extends Component {
 
   onSubmit = (e) => {
     e.preventDefault();
+    if (this.state.name.length < 2 || !this.state.salary) return
     this.props.onAdd(this.state.name, this.state.salary);
     this.setState({
       name: '',
@@ -31,24 +33,31 @@ class EmployersAddForm extends Component {
   render() {
     const { name, salary } = this.state;
 
+    // const inputId = useId()
+
     return (
       <div className="app-add-form">
         <h3>Добавьте нового сотрудника</h3>
         <form
           className="add-form d-flex"
           onSubmit={this.onSubmit}>
-          <input type="text"
-            className="form-control new-post-label"
-            placeholder="Как его зовут?"
-            name="name"
-            value={name}
-            onChange={this.onValueChange} />
-          <input type="number"
-            className="form-control new-post-label"
-            placeholder="З/П в $?"
-            name="salary"
-            value={salary}
-            onChange={this.onValueChange} />
+          <label>
+            <input type="text"
+              className="form-control new-post-label"
+              placeholder="Как его зовут?"
+              name="name"
+              value={name}
+              onChange={this.onValueChange} />
+          </label>
+          <label>
+            <input type="number"
+              className="form-control new-post-label"
+              placeholder="З/П в $?"
+              name="salary"
+              value={salary}
+              onChange={this.onValueChange} />
+          </label>
+
 
           <button type="submit"
             className="btn btn-outline-light">Добавить</button>
